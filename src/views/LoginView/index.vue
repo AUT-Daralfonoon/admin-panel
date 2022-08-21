9337b320c5a49cbbe05eed536f6a29425d1a1fab
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-21 10:02:16 +0430

    update: login style

--- src/views/LoginView/index.vue
+++ src/views/LoginView/index.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <InputField placeholder="username" label="User" input-type="text" />
-    <InputField placeholder="password" label="Pass" input-type="password" />
+  <div class="login-container">
+    <InputField class="login-items" placeholder="username" label="User" input-type="text" />
+    <InputField class="login-items" placeholder="password" label="Pass" input-type="password" />
+    <button class="login-items">
+      Login
+    </button>
   </div>
 </template>
 
@@ -9,10 +12,26 @@
 import InputField from "./components/InputField.vue";
 export default {
   name: "index",
-  components: {InputField}
+  components: { InputField }
 }
 </script>
 
 <style scoped>
+.login-container {
+  margin: 250px auto 0;
+  padding: 40px 20px;
+  width: 290px;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  border: 1px solid #9f9f9f;
+  border-radius: 5px;
+}
+
+.login-items {
+  margin-bottom: 5px;
+}
 </style>
